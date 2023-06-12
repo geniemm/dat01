d1cfd97fda4f6dcd57e98be8c50dd6577eff8c5f
myung <mong_jini@naver.com>
2023-06-12 14:25:21 +0900

    day01플젝 생성

---
+++ src/day01/ClassTest01.java
@@ -0,0 +1,31 @@
+package day01;
+
+public class ClassTest01 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(100);
+		System.out.println("안녕하세요");
+		//System.out.println('a'); 
+		// 괄호안의 내용을 출력해주는 기능
+		// ; : 쎄미콜론이며 문장의 마지막에는 꼭 있어야 한다.
+		
+		System.out.print("안녕");
+		System.out.print("하세");
+		System.out.print("요"); 
+		// ctrl + alt + 방향키 : 같은내용 복사
+		
+		System.out.println();
+		for (int i=0;i<5;i++) {
+			for(int j=0;j<=i;j++) {
+			System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+	}
+	
+	
+	
+}
